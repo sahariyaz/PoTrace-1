b5616ff5769ab32934c84b5be187211fef10bad1
William Bergeron-Drouin <william@inter-net.ca>
2020-04-06 17:39:56 -0400

    Renamed from PoTrace to Potrace

--- dotnet-core/Potrace.cs
+++ dotnet-core/Potrace.cs
@@ -14,10 +14,10 @@ using System.Drawing.Imaging;
 // This program is distributed in the hope that it will be useful, but  WITHOUT ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  General Public License for more details.
 // You should have received a copy of the GNU General Public License  along with this program; if not, write to the Free Software  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,  USA. 
 
-namespace PoTrace
+namespace Potrace
 {
 
-    public partial class PoTrace
+    public partial class Potrace
     {
 
         #region additional Class definitions
